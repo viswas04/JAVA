b27d3a53a0a4c0cc1ae9d978f62d64c433205b80
venkataviswasmolugu@gmail.com <venkataviswasmolugu@gmail.com>
2025-08-23 23:50:42 +0530

    ...

--- METHODS/Employee.java
+++ METHODS/Employee.java
@@ -2,22 +2,24 @@ public class Employee {
 	int empAge;
 	double empSalary;
 	double empBonus;
+	int empId;
 
 	public Employee(int empAge, double empSalary) {
 		this.empAge = empAge;
 		this.empSalary = empSalary;
 	}
 
-	public Employee(double empSalary, double empBonus) {
+	public Employee(double empSalary, double empBonus,int empId ) {
 		this.empSalary = empSalary;
 		this.empBonus = empBonus;
+		this.empId = empId;
 
 	}
 
 	public static void main(String[] args) {
 		Employee e1 = new Employee(22, 23000.0);
 		e1.siddu();
-		Employee e2 = new Employee(23000.00, 1000.00);
+		Employee e2 = new Employee(23000.00, 1000.00,21);
 		e2.siddu();
 	}
 
@@ -26,6 +28,7 @@ public class Employee {
 		System.out.println("Emp age is :" + empAge);
 		System.out.println("Emp sal is :" + empSalary);
 		System.out.println("Emp bonus is:"+empBonus);
+		System.out.println("Emp Id is:"+empId);
 	}
 	
 }
