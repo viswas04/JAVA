2e75e0c23a0a15889973957a664d72fe751c4938
venkataviswasmolugu@gmail.com <venkataviswasmolugu@gmail.com>
2025-08-28 23:41:55 +0530

    ...

---
+++ CONTROL_STATEMENTS/HRDep.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class HRDep {
+	
+	public static void main(String[] args) {
+		//Scanner sc = new Scanner(System.in);
+		//System.out.println("Enter the Employee name : ");
+		//String name = sc.nextLine();
+		System.out.println("Enter years of Experience :");
+		int exp = sc.nextInt();
+		if (exp > 10) {
+			System.out.println("Your bonus is : 20000");
+		} else if (exp > 9) {
+			System.out.println("Your bonus is : 10000");
+		} else if (exp > 4) {
+			System.out.println("Your bonus is : 5000");
+		} else if (exp >=0 && exp <=1) {
+			System.out.println("Your bonus is : 1000");
+		}
+		else {
+			System.out.println("Invalid experience entered!");
+		}
+		System.out.println("Employee Name is :" + name);
+		System.out.println("Years of Experience : " + exp);
+		sc.close();
+	}
+
+}
