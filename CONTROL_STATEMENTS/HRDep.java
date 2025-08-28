a6dedbed9490b94d89d39de0db81af451d210111
venkataviswasmolugu@gmail.com <venkataviswasmolugu@gmail.com>
2025-08-28 23:42:15 +0530

    ...

--- CONTROL_STATEMENTS/HRDep.java
+++ CONTROL_STATEMENTS/HRDep.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class HRDep {
 	
 	public static void main(String[] args) {
-		//Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		//System.out.println("Enter the Employee name : ");
 		//String name = sc.nextLine();
 		System.out.println("Enter years of Experience :");
