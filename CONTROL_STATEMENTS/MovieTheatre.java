9dd3daa3ceff10746803ea2fad1e332eb7334692
venkataviswasmolugu@gmail.com <venkataviswasmolugu@gmail.com>
2025-09-01 23:47:46 +0530

    ...

--- CONTROL_STATEMENTS/MovieTheatre.java
+++ CONTROL_STATEMENTS/MovieTheatre.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class MovieTheatre {
 
 	public static void main(String[] args) {
-		//Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter show : ");
 		String showtime = sc.next();
 		int actualprice = 0;
